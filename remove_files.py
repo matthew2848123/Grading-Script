1c3e290a59d9ae474667599de70d0639518241e2
matthew2848123 <repecki88@gmail.com>
2021-09-08 16:51:45 -0400

    removed debugging stuff

--- remove_files.py
+++ remove_files.py
@@ -3,25 +3,22 @@ import shutil
 def remove_files(file_directory,names,assignment_name):
     os.chdir(file_directory)
     files = os.listdir()
-    #print(files)
     for i in range(len(files)):
         a = files[i].split("-")
         try:
             if(a[3] in names):
                 print("include")
             else:
-                #print("removing file")
                 path = assignment_name + "-" + str(a[3])
                 print(path)
                 shutil.rmtree(path)
         except:
             #print("invalid file" + str(a))
-            #print("got an error")
             pass
     
 def main():
-    directory = input("Please enter directory")
-    assignment = input("Please enter assignment name")
+    directory = input("Please enter directory: ")
+    assignment = input("Please enter assignment name: ")
     names = [] #Enter Students github usernames seperated by commas, ex "mrr5298","matthew2848123",...
     remove_files(directory,names,assignment)
 main()
